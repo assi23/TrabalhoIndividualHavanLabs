dcbbb9712ba2185e8b8001e71f8d48788c96b911
assi23 <guiassi@hotmail.com>
2021-07-16 15:22:19 -0300

    trabalho prático

---
+++ TrabalhoIndividual/TrabalhoIndividual/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoIndividual
+{
+	class Pessoa
+	{
+		public string Nome { get; set; }
+		public DateTime DataNasc { get; set; }
+	}
+}
---
+++ TrabalhoIndividual/TrabalhoIndividual/PessoaJuridica.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoIndividual
+{
+	class PessoaJuridica : Pessoa
+	{
+		public string IE { get; set; }
+		public string CNPJ { get; set; }
+
+		public Endereco endereco { get; set; }
+	}
+}
